e1f738b1c86e58f592cd6e47db0705cdf74d68e6
Tushar <tus123kumar@gmail.com>
2025-10-08 10:42:48 +0530

    merged with main

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // add new feature 
 // added dot ....
+
+// added button 
+//  added form 
+//  added form 
+
+// this is new feature added in branch feature ...
+
+//  from  main branch 
+// from feature branch 
+
+// toggle btn made 
+
+// edit btn js is added 
+
+// added submit form functionality.
